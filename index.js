a14d7e1afa664e68acccec7a3a5ba6fd7d63768a
Dan Storey <danielstorey@gmail.com>
2022-09-02 09:14:46 +0100

    add interactive cli and delete selected branches

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
 
-console.log('GIT UNBRANCH!');
+const {execSync} = require("child_process");
+const { MultiSelect } = require('enquirer');
+
+const branches = execSync('git branch | grep -v "*"')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(str => str.trim())
+
+const prompt = new MultiSelect({
+    message: 'Which branches do you wish to delete?',
+    choices: branches
+});
+
+prompt.run().then(branchesToDelete => {
+    const branchesStr = branchesToDelete.join(' ');
+    execSync(`git branch -D ${branchesStr}`)
+})
